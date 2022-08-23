c02139faad88d0608496b5b4f28263b0ac0e2add
ictu-dungnt98 <ntdungembedded98@gmail.com>
2022-08-23 16:41:16 +0700

    porting... run success

--- examples/raw_dumper/main/raw_dumper.c
+++ examples/raw_dumper/main/raw_dumper.c
@@ -7,9 +7,12 @@
 #include <ir/raw.h>
 
 
-#define IR_RX_GPIO 12
+#define IR_RX_GPIO 5
 
 void ir_dump_task(void *arg) {
+
+	printf("dungnt98 ir raw dumper\n");
+
     ir_rx_init(IR_RX_GPIO, 1024);
     ir_decoder_t *raw_decoder = ir_raw_make_decoder();
 
@@ -17,8 +20,9 @@ void ir_dump_task(void *arg) {
     int16_t *buffer = malloc(buffer_size);
     while (1) {
         int size = ir_recv(raw_decoder, 0, buffer, buffer_size);
-        if (size <= 0)
+        if (size <= 0) {
             continue;
+		}
 
         printf("Decoded packet (size = %d):\n", size);
         for (int i=0; i < size; i++) {
@@ -31,7 +35,15 @@ void ir_dump_task(void *arg) {
             printf("\n");
     }
 }
+
+void ir_receiver_task(void *arg) {
+    while (1) {
+		ir_rx_loop();
+		vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 void app_main()
 {
     xTaskCreate(ir_dump_task, "IR dump", 2048, NULL, tskIDLE_PRIORITY, NULL);
+	xTaskCreate(ir_receiver_task, "IR receiver", 2048, NULL, tskIDLE_PRIORITY, NULL);
 }
